2bbf1012b3084d984646ac7779587e6e9a455b5a
JeremyKRay <98500639+JeremyKRay@users.noreply.github.com>
2022-10-19 11:10:14 -0400

    committing minor change to code. Ultimately did not use in the final code and commented out

--- Script/MechaCarChallenge.R
+++ Script/MechaCarChallenge.R
@@ -2,12 +2,19 @@
 # Use the library() function to load the dplyr package
 library(dplyr)
 # Import and read the MechaCar_mpg.csv file as dataframe
-MechaCar_mpg <- read.csv(file = 'MechaCar_mpg.csv', check.names = F,stringsAsFactors = F)
+MechaCar_mpg <- read.csv(file = 'c:/users/jerem/class/analysisprojects/Module_15/R_Analysis/Module 15 Challenge/MechaCar_Statistical_Analysis/Data/MechaCar_mpg.csv', check.names = F,stringsAsFactors = F)
 # Perform linear regression using the lm() function. In the lm() function, pass in all six variables (i.e., columns), and add the dataframe you created in Step 4 as the data parameter.
 # MPG is the Dependent Variable and vehicle_length, vehicle_weight, spoiler_angle, ground_clearance, and AWD are the independent variables
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
 # Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
 summary(lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg))
+
+# Removed the Vehicle_length and ground_clearance
+# Perform linear regression using the lm() function. In the lm() function, pass in 4 variables (i.e., columns), and add the dataframe you created in Step 4 as the data parameter.
+# MPG is the Dependent Variable and vehicle_weight, spoiler_angle, and AWD are the independent variables
+# lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
+# Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
+# summary(lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg))
 
 # Deliverable 2
 # Import and read in the suspension_coil.csv file as a table
@@ -25,4 +32,4 @@ t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot1")$PSI,mu=1500)
 t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot2")$PSI,mu=1500)
 t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot3")$PSI,mu=1500)
 
-?t.test()
+
